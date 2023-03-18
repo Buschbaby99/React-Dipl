09c64c41f46dda952eca897bb29820fb23bac0c2
Mario Pilz <mario.pilz0601@gmail.com>
2023-03-18 12:13:56 +0100

    Navbar change

--- resources/js/Components/Admin/NavbarComponent.jsx
+++ resources/js/Components/Admin/NavbarComponent.jsx
@@ -4,6 +4,8 @@ import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import firmenlogo from "../../../images/firmenlogo.png";
 import Dropdown from "@/Components/Inputs/Dropdown";
 import SettingsIcon from "@mui/icons-material/Settings";
+import ProjectsDropdown from "@/Components/Buttons/ProjectsDropdown";
+
 
 const navigation = [
     { name: "News", href: "NewsPage", current: false },
@@ -59,25 +61,35 @@ export default function MyNavbar({ myUser = NULL, header, children }) {
                                 </div>
                                 <div className="hidden sm:ml-6 sm:block">
                                     <div className="flex space-x-4">
-                                        {navigation.map((item) => (
-                                            <a
-                                                key={item.name}
-                                                href={item.href}
-                                                className={classNames(
-                                                    item.current
-                                                        ? "bg-gray-900 text-white"
-                                                        : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                                                    "px-3 py-2 rounded-md text-sm font-medium"
-                                                )}
-                                                aria-current={
-                                                    item.current
-                                                        ? "page"
-                                                        : undefined
-                                                }
-                                            >
-                                                {item.name}
-                                            </a>
-                                        ))}
+                                        {navigation.map((item) => {
+                                            if (item.name === "Projekte") {
+                                                return (
+                                                    <ProjectsDropdown
+                                                        key={item.name}
+                                                    />
+                                                );
+                                            } else {
+                                                return (
+                                                    <a
+                                                        key={item.name}
+                                                        href={item.href}
+                                                        className={classNames(
+                                                            item.current
+                                                                ? "bg-gray-900 text-white"
+                                                                : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                                                            "px-3 py-2 rounded-md text-sm font-medium"
+                                                        )}
+                                                        aria-current={
+                                                            item.current
+                                                                ? "page"
+                                                                : undefined
+                                                        }
+                                                    >
+                                                        {item.name}
+                                                    </a>
+                                                );
+                                            }
+                                        })}
                                     </div>
                                 </div>
                             </div>
