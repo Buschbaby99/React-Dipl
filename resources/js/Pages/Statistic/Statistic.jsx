bf999af4c74cff1c68b6400cda2f7ceb0c96d4f2
Mario Pilz <mario.pilz0601@gmail.com>
2023-03-19 08:45:24 +0100

    working on Statistic

---
+++ resources/js/Pages/Statistic/Statistic.jsx
@@ -0,0 +1,24 @@
+import { Head } from '@inertiajs/react';
+import LoggedIn from '@/Components/ChangeNavbarComponent';
+import ProjectTable from '../../Components/Projects/ProjectTableComponent';
+import AdminButton from '@/Components/Buttons/UniversalButtonComponent';
+import ProjectsBarChartComponent from '@/Components/Projects/ProjectsBarChartComponent'
+
+export default function myprojects(props) {
+    
+    console.log(props);
+    return (
+        <div>
+            <Head title="Statistic" />
+            <LoggedIn auth={props.auth}></LoggedIn>
+            <AdminButton 
+                href='ProjectInsertPage'
+                text="Hinzufügen">
+            </AdminButton>
+            <ProjectTable props={props}></ProjectTable>
+            <ProjectsBarChartComponent></ProjectsBarChartComponent>
+        </div>
+    );
+
+}
+
