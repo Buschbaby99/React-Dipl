cf0fc9a71c0c8da708c46b664e747bff91e37f68
Mario Pilz <mario.pilz0601@gmail.com>
2023-03-24 12:49:30 +0100

    Ressoures finished

--- resources/js/Pages/Statistic/Statistic.jsx
+++ resources/js/Pages/Statistic/Statistic.jsx
@@ -1,7 +1,7 @@
 import { Head } from '@inertiajs/react';
 import LoggedIn from '@/Components/ChangeNavbarComponent';
 import ProjectsBarChartComponent from '@/Components/Statistics/ProjectsBarChartComponent';
-import PersonsStatistic from '@/Components/Statistics/PersonsStatistic';
+import PersonsStatistic from '@/Components/Statistics/EmployeeProjectInfo';
 import DepartmentsStatistic from '@/Components/Statistics/DepartmentsStatistic';
 
 export default function myprojects(props) {
@@ -12,8 +12,8 @@ export default function myprojects(props) {
             <Head title="Statistic" />
             <LoggedIn auth={props.auth}></LoggedIn>
             <ProjectsBarChartComponent></ProjectsBarChartComponent>
-            <PersonsStatistic></PersonsStatistic>
             <DepartmentsStatistic></DepartmentsStatistic>
+            <PersonsStatistic></PersonsStatistic>
             
         </div>
     );
