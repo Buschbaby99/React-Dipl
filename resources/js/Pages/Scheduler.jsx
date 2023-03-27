4dc7722876e58e474c7d4c1704fa70a81bf9cec4
Mario Pilz <mario.pilz0601@gmail.com>
2023-03-27 08:18:38 +0200

    First idea of responsive scheduler

--- resources/js/Pages/Scheduler.jsx
+++ resources/js/Pages/Scheduler.jsx
@@ -1,22 +1,54 @@
-import LoggedIn from '@/Components/ChangeNavbarComponent';
-import { Head } from '@inertiajs/react';
-import SchedulerComponent from '@/Components/Scheduler/SchedulerComponent';
+import React, { useState, useEffect } from "react";
+import LoggedIn from "@/Components/ChangeNavbarComponent";
+import { Head } from "@inertiajs/react";
+import SchedulerComponent from "@/Components/Scheduler/SchedulerComponent";
+import MobileSchedulerComponent from "@/Components/Scheduler/MobileScheduler";
 
 export default function Scheduler(props) {
-  
-   // console.log(JSON.parse(props.data));
+    // console.log(JSON.parse(props.data));
 
-   
-   
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const checkViewportWidth = () => {
+            if (window.innerWidth <= 768) {
+                // Sie können diesen Wert anpassen, um Ihre eigene Definition von "mobil" festzulegen.
+                setIsMobile(true);
+            } else {
+                setIsMobile(false);
+            }
+        };
+
+        checkViewportWidth();
+        window.addEventListener("resize", checkViewportWidth);
+
+        return () => {
+            window.removeEventListener("resize", checkViewportWidth);
+        };
+    }, []);
+
     return (
         <div>
             <Head title="Scheduler" />
 
             <LoggedIn auth={props.auth}></LoggedIn>
             <header className="bg-white shadow">
-                    <div className="max-w-7xl mx-auto  text-xl py-6 px-4 sm:px-6 lg:px-8">{"Personaleinteilung"}</div>
-                </header>
-            <SchedulerComponent data={props.data} projects={props.projects}></SchedulerComponent>
+                <div className="max-w-7xl mx-auto text-xl py-6 px-4 sm:px-6 lg:px-8">
+                    {"Personaleinteilung"}
+                </div>
+            </header>
+            {isMobile ? (
+                <MobileSchedulerComponent
+                    data={props.data}
+                    projects={props.projects}
+                    auth={props.auth}
+                ></MobileSchedulerComponent>
+            ) : (
+                <SchedulerComponent
+                    data={props.data}
+                    projects={props.projects}
+                ></SchedulerComponent>
+            )}
         </div>
     );
 }
